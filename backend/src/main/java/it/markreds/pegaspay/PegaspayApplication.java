69e2365bfe17182fa8b0bd764cabb7d6057cc23a
Marco <mark@MacBookPro.homenet.telecomitalia.it>
2025-07-12 22:26:03 +0200

    Initial commit

---
+++ backend/src/main/java/it/markreds/pegaspay/PegaspayApplication.java
@@ -0,0 +1,13 @@
+package it.markreds.pegaspay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegaspayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegaspayApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/it/markreds/pegaspay/PegaspayApplicationTests.java
@@ -0,0 +1,13 @@
+package it.markreds.pegaspay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegaspayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
