7d313f0304911f51aa060b2fb5c62cda005d92b0
Mark Reds <marco@markreds.it>
2025-08-14 20:11:54 +0200

    Corretta la gestione del segno tra i valori debit e credit. Algoritmo di generazione dei codici di riscatto più robusto.

--- backend/src/main/java/it/markreds/pegaspay/util/CodeGenerator.java
+++ backend/src/main/java/it/markreds/pegaspay/util/CodeGenerator.java
@@ -2,6 +2,12 @@ package it.markreds.pegaspay.util;
 
 import java.security.SecureRandom;
 
+/**
+ * Generatore di codici basato sull'algoritmo Base32 Crockford con  le seguenti caratteristiche:
+ * - genera codici lunghi aumentando l'entropia e quindi la sicurezza
+ * - leggibile, evitando caratteri simili tipo 0/O, 1/I/l, ecc.
+ * - facile da trascrivere, raggruppando in blocchi
+ */
 public class CodeGenerator {
     private static final String ALPHABET = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
     private static final SecureRandom RANDOM = new SecureRandom();
